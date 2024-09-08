0dd430d982622a4a27e5403c02eda902a9492c81
Cale Kochenour <calekochenour@gmail.com>
2024-09-08 12:55:39 -0600

    Add initial structure changes for iteration 1

--- eXit.py
+++ eXit.py
@@ -1,142 +1,149 @@
-#!/bin/python3
+"""
+ **Answers to each input:*** (to follow episode 4x11)
 
-# **Answers to each input:***
-# move barrel
-# enter tunnel
-# read note
-# leave
-# look
-# get on boat
+ Iteration 1:
+   - Move the barrel
+   - Enter tunnel
+   - Read note
+   - Leave
+   - Look
+   - Get on the boat
+   - Yes
 
-
-import os
-import sys
-
-ascii = '''
-████████████████████████████████████████████████▓▓▓▓▓▓▓▓█████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▓╨╨╨╨╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╙╟▓▓██▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌``¡≥≥▒▒▒≥≥≥▒≥≥≥▒▒▒Q░░░░░▒Q░Q░▒▒▒▒▒≥▒≥▒▒≥≥≥≥░?^╠╫▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌--░╠╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╣╣╬╬╬╬╬╬╬╬╬╢╬╬╠╠╠╠▒▒▒╫▒--╠╫██▓▓▓▓▓▓▓▓▓███
-█████████████████▌(ⁿ;╠╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╠╠╠╠╠╠╠╠╬▒ƒⁿ╠╣▓▓█████▓▓█████
-█████████████████▌-,»░╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▒▒▒╬▒Γ,░╬▓▓██▓▓▓▓▓█████
-█████████████████▌⌐¬;╠╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╩╩╩╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬░⌐¬╚╣▓▓▓▓▓▓▓▓▓██▓▓▓
-█████████████████▌""7▒╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬∩. .╠╬╬╬╬╬╬╬╬╬╬▒▒▒▒▒▒▒▒╬▒b"≤╟█▓▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌~~~┘~╙╙╙╙╙╙╙║╫╫╫╫░~~~~~║╫╫╫╫╢╢╢╢╢╠▒▒▒▒▒▒▒▒╫▒~~5╫▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌`````````````╙╙╙╙?````^╠╢╬╬╬╬╬╬╬╠╠▒▒▒▒▒▒▒▒╬▒░`5╫▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌-----------------------║╫╫╬╢╢╢╢╢╠▒▒▒▒▒▒▒▒▒╫▒--░╬╬╣▓▓▓▓▓████▓▓▓
-█████████████████▌;;;ⁿ                 ,ƒ╠╬╬╬╬╬╬╬╬╬╠╠╠╠╠╠╠╠░╬▒░;░╠╬╬╬╬▓██████▓▓▓
-█████████████████▓ΓΓΓΓ                 '░╠╬╬╬╬╬╬╬╬╬▒▒▒▒▒▒▒▒▒╬▒ΓΓ░╬╬╬╬╬█████▓▓▓▓▓
-█████████████████▌¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬;╠╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╚░╬░⌐¬░╟████▓▓▓██▓▓▓▓╬
-█████████████████▌"""". .........  ...777╠╬╬╬╬╬╬╬▒▒▒▒▒▒▒▒▒▒ñ╠▒""ñ╟▓▓▓▓██▓▓▓▓▓╬╬╬
-█████████████████▌~~~~~~~~~~~~~~~~~~~»»~∞║╫╫╫╢╢╢╢▒▒▒▒▒▒▒▒▒░░╠░~~░╫▓▓██▓▓▓▓▓╫╫▓██
-█████████████████▌````````````````??????░╟▓╫╬╬╬╬╬▒▒▒▒▒▒▒▒▒▒▒╠▒░`3╫██▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌----------------------Å╫█▓╫╢╢╢▒▒▒▒▒▒▒▒▒░░░╠▒--░╫▓▓██▓▓▓▓▓▓▓▓▓▓
-█████████████████▌;;;ⁿ          ;ƒƒƒƒƒƒ;≥╟█▓╬╬╬╬╠╠╠╠╠╠▒░╠░░░╠░ƒ;░╟██▓▓▓▓▓▓▓▓▓▓▓▓
-█████████████████▌----          -======░╚╟▓▓╬╬╬╬▒▒▒▒▒╩▒░▒░░░╠▒=-░╟██▓▓███▓▓▓▓▓▓▓
-█████████████████▌¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬j╠╬╬╬╬╬╬╬╬╬╬╩▒░░╩░░░╠░⌐¬░╟███████▓▓▓▓▓█▓
-█████████████████▌........""7.:"""""""""l╠╬╬╬╬╬╬╬╬▒▒▒▒▒▒▒▒▒▒╠▒".⌠╟███████▓▓▓▓▓▓▓
-█████████████████▌~~~~»»~~~~~~~~~~~~~ⁿⁿⁿⁿ║╢╢╢╢╢╢▒╩▒▒╚╙░░░░░░╠░~~≤╫███████▓▓▓▓▓▓▓
-█████████████████▌^^^^?????????????¿````^╠╬╬╬╬▒▒▒▒▒▒▒▒▒▒▒▒▒▒╠▒░^5╫█████▓▓▓▓▓▓▓▓▓
-█████████████████▌----~~------------~~~~-║╫╢╢╢╢╠╠╠╠╠╚╚╚╚╚▒░▒╠▒--░╫█████████▓▓╬╢╢
-█████████████████▌;;;;,                ,;╠╬╬╬╬╠╠╠╠╠░░░░░░░░░╠░░;╠╫█████████▓▓╬╬╬
-█████████████████▌-----------------------╠╬╬╬╬╬▒▒▒▒▒░░░░░░░░╠░░-░╣███████▓▓█████
-█████████████████▌¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬╠╬╬╬╬╬╬╬╬╬░░░░░░░░░╠░¬¬╠╣██████████████
-█████████████████▌.....                ..╠╬╬╬╬╬▒▒▒▒▒▒▒▒▒▒▒▒▒╠▒..ñ╟██████████████
-█████████████████▌~~~~~~~~~~~~~~~~~~~~~~~╠╫╫╬╢╠╠▒▒▒▒▒░░░░░░░╠░~~░╫██████████████
-█████████████████▌^^^^``````````````````^╠╬╬╬╬╬╬╠▒▒▒▒▒▒▒▒▒▒▒╠▒^^3╫████████████▓▓
-█████████████████▌----──────────────────-║╫╫╫╫╫╫╫╫▒▒▒▒▒▒▒░░░╠▒--¡╫██████████████
-██████████████████░((;ⁿⁿ;ⁿ;(;ⁿⁿ((;ⁿⁿⁿⁿⁿ;░╟▓▓▓▓▓╬╬╬╬╬╬╬╬╠╠░░░╠░░(░╫██████████████
-██████████████████░---------------------░╟▓▓▓▓███▓▓▓▓╬╬╬╬╬▒▒╬░░-░╟██████████████
-██████████████████▒╔╔╦▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓▓▓▓▓▓▓▓▓████████▓▓▓▒Q╔╔╣██████████████
-████████████████████████████████████████████████████████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-███████████████████████████████▓╫╫╫╫█████╫╫╫╫▓██████████████████████████████████
-███████████████████╬╬╬╬╬╬╬╬█████╬╬╬╬╬╬╬╬╬╬╬╬╣█████████████╬╬╬╬██████████████████
-█████████████████╬╬╬▓███▓╬╬╬╣████▓╬╬╬╬╬╬╬╬╬███████▓╬╬╬╬██▓▓╬╬╬╬╣▓▓██████████████
-██████████████╬╬╬╬╬╬╬╬╬╬╬╬╬╬╣████▓╬╬╬╬╬╬╬╬╬████████╬╬╬╬╬█████╬╬╬╬███████████████
-█████████████╬╬╬╬▓███████████████╬╬╬╬╬╬╬╬╬╬╬███████▓╬╬╬╬█████▓╬╬╬╬╬█████████████
-███████████▓╬╬╬╬╬███╬╬╬╬╬╣█████╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██████╬╬╬╬╬╬████▓╬╬╬╬╣████████████
-██████████╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓████╬╬╬╬╬╬╬▓██╬╬╬╬╬╬╬╬█████╬╬╬╬╬╬██████▓╬╬╬╬╬██████████
-█████████████╫╫╫╫╫╫╫██████╬╫╫╫╫╫╫╫╫█████▓╫╫╫╫╫╫╫╢█████╫╫╫╫╫████████╫╫╫╫╫╫╫╢█████
-██████████████████████████╬╬╬╬╬╬╬▓███████▓╬╬╬╬╬╬╬██████████████████████▓▓▓██████
-███████████████████████▓╬╬╬╬╬╬╬▓██████████╬╬╬╬╬╬╬╬╬╬████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-'''
-print(ascii)
+ Iteration 2:
+   - Sit down next to my friend
+   - Light a match
+   - Stay
+"""
 
 import os
 import sys
 
-def congrats():
-    print("Congratulations, you're heading to a new world!.\nDo you want to play again?") 
+import ascii_images
+
+
+def congratulations():
+    print(
+        "\nCongratulations, you're heading to a new world!"
+        "\nDo you want to play again?\n"
+    )
     if input("> ").lower() == "yes":
         os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
     else:
         quit()
 
-def startagain():
-    input('Press the enter button to continue...')
 
-def trapped():
-    print("You're trapped in a dungeon with your friend.\nYou see a barrel. What do you do?")
+def start():
+    print(ascii_images.start)
+    input('Press the "Enter" button to start...')
 
-def dungeon():
+
+# def trapped():
+#     print(
+#         "\nYou're trapped in a dungeon with your friend."
+#         "\nYou see a barrel. What do you do?"
+#     )
+
+
+def barrel():
+    print(
+        "\nYou're trapped in a dungeon with your friend."
+        "\nYou see a barrel. What do you do?\n"
+    )
     while True:
-        if input("> ").lower() == "move barrel":
-            print("The barrel rolls aside and you find a secret tunnel.\nWhat do you do?")
-            break  # Break the loop if condition is met
+        if input("> ").lower() == "move the barrel":
+            break
         else:
-            print("You can't do that here.")
+            print("You can't do that here.\n")
 
-def bye():
+
+def tunnel():
+    print(
+        "\nThe barrel rolls aside and you find a secret tunnel."
+        "\nWhat do you do?\n"
+    )
     while True:
         if input("> ").lower() == "enter tunnel":
-            print("You start to escape but your friend is too weak to go with you. They hand you a note.\nWhat do you do?")
             break
         else:
-            print("You can't type that.")
+            print("You can't do that here.\n")
+
 
 def note():
+    print(
+        "\nYou start to escape but your friend is too weak to"
+        "\ngo with you. They hand you a note."
+        "\nWhat do you do?\n"
+    )
     while True:
         if input("> ").lower() == "read note":
-            print("It is too dark to read the note.\nWhat do you do?")
             break
         else:
-            print("You can't do that here.")
+            print("You can't do that here.\n")
 
-def leave():
+
+def too_dark():
+    print("\nIt is too dark to read the note. " "\nWhat do you do?\n")
     while True:
         if input("> ").lower() == "leave":
-            print("You crawl through the tunnel and the tunnel leads to a beach.\nWhat do you do?")
             break
         else:
-            print("You can't do that here.")
+            print("You can't do that here.\n")
 
-def look():
+
+def leave():
+    print(
+        "\nYou crawl through the tunnel and the tunnel leads"
+        "\nyou to a beach. What do you do?\n"
+    )
     while True:
         if input("> ").lower() == "look":
-            print("In the water you see a boat.\nWhat do you do?")
             break
         else:
-            print("You can't do that here.")
+            print("You can't do that here.\n")
 
-def get():
+
+def look():
+    print("\nIn the water you see a boat." "\nWhat do you do?\n")
     while True:
-        if input("> ").lower() == "get on boat":
-            congrats()
-            break
+        if input("> ").lower() == "get on the boat":
+            # break
+            congratulations()
         else:
-            print("You can't do that here.")
+            print("You can't do that here.\n")
 
-# Initial function calls to start the story
-startagain()
-trapped()
-dungeon()
-bye()
-note()
-leave()
-look()
-get()
+
+def stay():
+    while True:
+        if input("\n> ").lower() == "Sit down next to my friend":
+            print(
+                "\nThe barrel rolls aside and you find a secret tunnel."
+                "\nWhat do you do?"
+            )
+            break
+            # Move to iteration two - stay
+
+
+def main():
+    start()
+    barrel()
+    tunnel()
+    note()
+    too_dark()
+    leave()
+    look()
+    # If iteration_1:
+    # Else iteration_2
+    # bye()
+
+
+if __name__ == '__main__':
+    main()
+    # start()
+    # trapped()
+    # dungeon()
+    # bye()
+    # note()
+    # leave()
+    # look()
+    # get()
